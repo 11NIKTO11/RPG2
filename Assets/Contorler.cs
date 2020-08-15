a76d156d38f448d8ae6bea4d96704632e909a8ae
Martin <martin.bakos11@gmail.com>
2020-08-15 14:13:23 +0200

    movement controller separation
    Comments added

---
+++ Assets/Contorler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contorler : MonoBehaviour
+{
+    public Movement movement;
+    private float direction;
+    private float jump;
+    private bool crouch;
+
+    private float efficiecy = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+        jump = Input.GetAxis("Jump");
+        Debug.Log(jump);
+        crouch = Input.GetKey(KeyCode.S);
+
+    }
+
+    void FixedUpdate()
+    {
+        movement.Move(direction * efficiecy, jump * efficiecy, crouch);
+    }
+}
--- Assets/scipts/Movement.cs
+++ Assets/scipts/Movement.cs
@@ -3,32 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Transform))]
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D movableRigidbody;
     private Transform movableTransform;
     private CapsuleCollider2D movableCollider;
-    private float overlapRadius = 0.1f;
-    [SerializeField] private LayerMask whatIsGround;
 
-    [SerializeField] private PhysicsMaterial2D stationary;
-    [SerializeField] private PhysicsMaterial2D moving;
+    private readonly float overlapRadius = 0.1f; //radius for collider checking  checking 
+    public LayerMask GroundLayer; //define whitch layer is ground
+    [SerializeField] private PhysicsMaterial2D stationary; //what material is used when character is stationary
+    [SerializeField] private PhysicsMaterial2D moving; //what material is used when character  is moving
 
-    [Min(0)] [SerializeField] private float stationaryFriction = 10f;
-    [Min(0)] [SerializeField] private float movingFriction = 0f;
+    [SerializeField] private float velocityMultiplier = 5f; //adjust horizontal velocity
+    [SerializeField] private float jumpMultiplier = 400f; //adjust jump strength
+    [SerializeField] private float crouchSpeedMultiplier = 0.4f; //adjust horizontal velocity when crouching
+    [SerializeField] private float crouchJumpMultiplier = 1.5f; //adjust jump strength when crouching
+    [Range(0f, 1f)] [SerializeField] private float crouchHeightMultiplier = 0.5f; //how much cillider shrinks when crouched
 
-    [SerializeField] private float velocityMultiplier = 5f;
-    [SerializeField] private float jumpMultiplier = 400f;
-    [SerializeField] private float crouchSpeedMultiplier = 0.4f;
-    [SerializeField] private float crouchJumpMultiplier = 1.5f;
-    [Range(0f, 1f)] [SerializeField] private float crouchHeightMultiplier = 0.5f;
-    [Range(0f, 1f)] [SerializeField] public float Efficiety = 1f;
-
-    private Vector2 colliderSize;
-    private Vector2 colliderOffset;
-    private bool crouched = false;
+    private Vector2 colliderSize; // normal/original collider size
+    private Vector2 colliderOffset; // normal/original collider offset
+    private bool crouched = false; //indicates if object is crouched
 
     // Start is called before the first frame update
+    // Setting up poperties
     void Start()
     {
         movableRigidbody = GetComponent<Rigidbody2D>();
@@ -39,55 +39,79 @@ public class Movement : MonoBehaviour
         crouchHeightMultiplier = Math.Max(crouchHeightMultiplier, colliderSize.x / colliderSize.y);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    /// <summary>
+    /// checks if there is space around character head height
+    /// </summary>
+    /// <returns> returns true if u can stand up else false</returns>
+    private bool CanStandUp()
     {
-        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"), Input.GetKey(KeyCode.S));
+        return !Physics2D.OverlapCircle(
+            new Vector2(movableTransform.position.x, movableTransform.position.y + colliderOffset.y + (colliderSize.y / 2)),
+            overlapRadius, GroundLayer);
     }
 
+    /// <summary>
+    /// checks if there is ground beneath character character feet
+    /// </summary>
+    /// <returns> returns true if character is on ground else false</returns>
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(
+            new Vector2(movableTransform.position.x, movableTransform.position.y + colliderOffset.y - (colliderSize.y / 2)),
+            overlapRadius, GroundLayer);
+    }
+
+    /// <summary>
+    /// handlles transition in and out of crouch based on desired state
+    /// </summary>
+    /// <param name="crouch">desired state</param>
     private void Crouch(bool crouch)
     {
         if (!crouched && crouch)
         {
             movableCollider.size = new Vector2(colliderSize.x, colliderSize.y * crouchHeightMultiplier);
-            movableCollider.offset = colliderOffset + new Vector2(0, -colliderSize.y * (1 - crouchHeightMultiplier) / 2);
+            movableCollider.offset = colliderOffset + new Vector2(0, -colliderSize.y * (1 - crouchHeightMultiplier) / 2); 
             crouched = true;
         }
-        else if (crouched && !crouch && !Physics2D.OverlapCircle(new Vector2(movableTransform.position.x, movableTransform.position.y + colliderOffset.y + (colliderSize.y / 2)), overlapRadius, whatIsGround))
+        else if (crouched && !crouch && CanStandUp())
         {
             movableCollider.size = colliderSize;
-            movableCollider.offset = colliderOffset;
+            movableCollider.offset = colliderOffset;    
             crouched = false;
         }
     }
 
-    public void Move(float velocity, float jump, bool crouch)
+    /// <summary>
+    /// Move character based on input
+    /// </summary>
+    /// <param name="direction">horizontal direction of movement</param>
+    /// <param name="jump"></param>
+    /// <param name="crouch"></param>
+    public void Move(float direction, float jump, bool crouch)
     {
-        bool grounded = Physics2D.OverlapCircle(new Vector2(movableTransform.position.x, movableTransform.position.y + colliderOffset.y - (colliderSize.y / 2)), overlapRadius, whatIsGround);
+        bool grounded = IsGrounded();
 
         Crouch(crouch);
 
         if (grounded)
         {
-            if (crouch)
+            if (crouched)
             {
-                velocity *= crouchSpeedMultiplier;
+                direction *= crouchSpeedMultiplier;
                 jump *= crouchJumpMultiplier;
             }
-            movableRigidbody.AddForce(new Vector2(0, jump * jumpMultiplier));
+            movableRigidbody.AddForce(new Vector2(0, jump * jumpMultiplier)); 
         }
 
-        if ((velocity == 0)&&(grounded))
+        if ((direction == 0)&&(grounded))
         {
             movableRigidbody.sharedMaterial = stationary;
-            Debug.Log("stationary");
         }
         else
         {
             movableRigidbody.sharedMaterial = moving;
-            Debug.Log("moving");
         }
 
-        movableRigidbody.velocity = new Vector2(velocity * velocityMultiplier, movableRigidbody.velocity.y);
+        movableRigidbody.velocity = new Vector2(direction * velocityMultiplier, movableRigidbody.velocity.y);
     }
 }
